7f44768abb8e47eb437c81a3ca886ce12859c7ba
orimoyano16 <orianamoyano16@gmail.com>
2025-08-12 13:41:56 -0300

    Segunda asignacion lista.

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
 
-  // Esta línea está comentada. Deberías descomentarla para que funcione
-  // y cambiar el texto que se envía como respuesta.
+// Sin comentarios
 
-  //res.end('Poné lo que quieras acá');
+  res.end('Tarea entregada Moyano Oriana');
 });
 
 server.listen(3000, () => {
